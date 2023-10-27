9a9b7383882b4ca97126683605f158cef9e7716c
Deepshikha Choudhary <dch@charlietango.dk>
2023-10-27 14:09:42 +0200

    task - removed unnecessaryfile

--- lib/utils.ts
+++
@@ -1,6 +0,0 @@
-import clsx, { ClassValue } from "@/node_modules/clsx/clsx";
-import { twMerge } from "@/node_modules/tailwind-merge/dist/index";
- 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
